5b9181204933422db5fefab226d2887bcd9246d0
liangwenqi <wenqizai@qq.com>
2021-12-05 13:44:51 +0800

    init

---
+++ liaoxuefeng/list.py
@@ -0,0 +1,13 @@
+classmates = ['Michel', 'Bob', 'Tracy']
+print(classmates)
+
+print(len(classmates))
+
+print(classmates[1])
+
+# print(classmates[3])
+
+print(classmates[-3])
+# print(classmates[-4])
+
+# 插入
